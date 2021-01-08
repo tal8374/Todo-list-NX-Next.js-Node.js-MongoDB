3175ca14609ed04b3224290e7aa08896658f3cd7
Tal Profus <tal@ubeya.com>
2021-01-08 16:22:22 +0200

    #Adding material UI in _app and _documnet

--- apps/front/pages/_app.tsx
+++ apps/front/pages/_app.tsx
@@ -3,7 +3,13 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import Copyright from '../components/CopyRight';
 import './styles.css';
-import { Box } from '@material-ui/core';
+import Navbar from '../components/Navbar';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
+
+
+// context: https://www.netlify.com/blog/2020/12/01/using-react-context-for-state-management-in-next.js/
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,14 +19,19 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
-      <div className="app">
+    <ThemeProvider theme={theme}>
+        <Navbar/>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+      {/* <div className="app">
         <main>
+          <Navbar/>
           <Component {...pageProps} />
-          {/* <Box mt={5}>
-            <Copyright />
-          </Box> */}
+       
         </main>
-      </div>
+      </div> */}
     </>
   );
 }
